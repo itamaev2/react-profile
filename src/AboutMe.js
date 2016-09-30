fbe5d782028a093aa6a2b2738f4a72ccc8112070
itamaev2 <itamaev2@gmail.com>
2016-09-30 17:25:45 +0900

    Update the data of About me component to obtain from JSON

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import { Card, CardTitle, CardText } from 'material-ui';
+import { Card, CardTitle, CardText, CardActions, IconButton } from 'material-ui';
+import $ from 'jquery';
 
 export default class AboutMe extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '', comment: '' };
+  }
+
+  componentWillMount() {
+    $.getJSON('./json/about_me.json', function (data) {
+      this.setState(data);
+    }.bind(this));
+  }
+
   render() {
     const style = {
       height: 300,
@@ -14,10 +26,13 @@ export default class AboutMe extends React.Component {
     return (
       <div className="abount_me">
         <Card style={style}>
-          <CardTitle title="About me です。" />
+          <CardTitle title={this.state.name} />
           <CardText>
-            About meの説明です。
+            {this.state.comment}
           </CardText>
+          <CardActions>
+            <IconButton iconClassName="muidocs-icon-custom-github" />
+          </CardActions>
         </Card>
       </div>
     );
