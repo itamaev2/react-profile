810c725cf98bc8797796f53b876375f07979856a
itamaev2 <itamaev2@gmail.com>
2016-10-10 23:20:04 +0900

    Add Appbar to Header

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { FlatButton } from 'material-ui'
 import ContentStat from './ContentStatus'
+import AppBar from 'material-ui/AppBar';
+
 
 export default class Header extends React.Component {
   render() {
     return (
       <div className="header">
-        <FlatButton label="About me" onClick={ () => this.props.onContentsChange(ContentStat.AboutMe) } />
-        <FlatButton label="Status" onClick={ () => this.props.onContentsChange(ContentStat.Status) } />
+        <AppBar 
+          iconElementRight={
+            <div>
+              <FlatButton label="About me" onClick={ () => this.props.onContentsChange(ContentStat.AboutMe) } />
+              <FlatButton label="Status" onClick={ () => this.props.onContentsChange(ContentStat.Status) } />
+            </div>
+          }
+        />
       </div>
     );
   }
